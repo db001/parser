61609a1b7380b164b1de919d88725de614731016
Dave <dnbennett@hotmail.co.uk>
2016-10-09 19:20:00 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var http = require('http');
+
+var port = 8080;
+
+var obj = {};
+
+var server = http.createServer(function(req, res) {
+    var header = req.headers;
+    var os = header["user-agent"];
+    os = /\(([^)]+)\)/.exec(os);
+    obj["Operating Sysytem"] = os[1];
+    var lang = header["accept-language"].split(",");
+    obj["Language"] = lang[0];
+    obj["IP"] = header["x-forwarded-for"];
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(obj));
+});
+
+server.listen(port, function(){
+    console.log("Server listening");
+})
